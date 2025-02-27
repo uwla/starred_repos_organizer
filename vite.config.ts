05af471c93162ce333695f45e9aa1d3269c0db2b
uwla <uwlalinux@gmail.com>
2025-02-27 20:06:00 -0300

    perf: increase performance by using react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile"
 
 const defaultBasePath =
     process.env.NODE_ENV === "demo" ? "/starred_repos_organizer" : "/";
 
+const ReactCompilerConfig = {
+  target: '18'
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteSingleFile()],
+    plugins: [
+        react({
+            babel: {
+                plugins: [
+                    ['babel-plugin-react-compiler', ReactCompilerConfig],
+                ]
+            }
+        }),
+        viteSingleFile(),
+    ],
     base: process.env.VITE_BASE_PATH || defaultBasePath,
 });
