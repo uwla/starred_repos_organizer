a812f1969b040897825cfef7dcf1691c5344828a
uwla <uwlalinux@gmail.com>
2025-04-26 19:39:41 -0300

    ci: fix build failing because mock driver not implementing interface

--- src/storage/mockDriver.ts
+++ src/storage/mockDriver.ts
@@ -1,4 +1,4 @@
-import { StorageDriver, Repo } from "../types";
+import type { StorageDriver, Repo, Topic, TopicAliases } from "../types";
 import data from "../../user-data-sample.json";
 
 const repos = data.repo as Repo[];
@@ -25,12 +25,18 @@ const mockDriver: StorageDriver = {
     async deleteMany(_: Repo[]) {
         return true;
     },
-    async setAllowedTopics(_: string[]) {
+    async setAllowedTopics(_: Topic[]) {
         return true;
     },
     async getAllowedTopics() {
         return [];
-    }
+    },
+    async setTopicAliases(_: TopicAliases) {
+        return true
+    },
+    async getTopicAliases() {
+        return {}
+    },
 };
 
 export default mockDriver;
